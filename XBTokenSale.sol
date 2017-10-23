d5c25e89b1aad10ba61bd6e60bb33bd74e487efa
Jitendra Chittoda <jcchittoda@gmail.com>
2017-10-23 17:59:03 +0530

    Modified for Pre-Seed token distribution
    - 2,640,000 Tokens to be distributed 
    - 1400 Tokens including 40% discount will be distributed per ETH

--- XBTokenSale.sol
+++ XBTokenSale.sol
@@ -200,10 +200,10 @@ contract XBTokenSale is NonTradableToken, Pausable {
     address public wallet;
 
     // Total XB tokens for PreSale
-    uint256 public constant TOTAL_XB_TOKEN_FOR_PRE_SALE = 10000000 * (10**decimals); //10 million * 10^decimals
+    uint256 public constant TOTAL_XB_TOKEN_FOR_PRE_SALE = 2640000 * (10**decimals); //2,640,000 * 10^decimals
 
     // how many token units a buyer gets per ETH
-    uint256 public rate = 500; //500 XB tokens per ETH
+    uint256 public rate = 1400; //1400 XB tokens per ETH, including 40% discount
 
     // How many sold in PreSale
     uint256 public presaleSoldTokens = 0;
